129b965eb09653a1b6faa98e5cb7f2c09f94e4ef
Diomuzan <94070451+TheCodeifier@users.noreply.github.com>
2025-04-13 19:17:01 +0200

    Started with developing ARPR

---
+++ src/main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Product {
+    public String name;
+    public int priceExclusiveVAT;
+}
+
+class Country {
+    public String name;
+    public int VAT;
+}
+
+class VATCalculator {
+
+}
+
+class Main {
+    
+}

95b50eb75e31d40f56d0eb6ce3247b6569909430
Diomuzan <94070451+TheCodeifier@users.noreply.github.com>
2025-04-25 21:39:49 +0200

    Updated Main class

--- src/main.java
+++ src/main.java
@@ -1,16 +1,6 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-interface CurrencyConverter {
-    double convertToLocalCurrency(double priceInEuros, double exchangeRateToEuro);
-}
-
-class EuroToCurrencyConverter implements CurrencyConverter {
-    public double convertToLocalCurrency(double priceInEuros, double exchangeRateToEuro) {
-        return priceInEuros * exchangeRateToEuro;
-    }
-}
-
 class Product {
     private final String name;
     private final double priceExclusiveVAT;
@@ -29,39 +19,50 @@ class Product {
     }
 }
 
-class Country {
-    private final String name;
-    private final double vatPercentage;
-    private final String currencySymbol;
-    private final double exchangeRateToEuro;
-    private CurrencyConverter currencyConverter; 
+abstract class Country {
+    protected String name;
+    protected double vatPercentage;
+    protected String currencySymbol;
 
-    public Country(String name, double vatPercentage, String currencySymbol, double exchangeRateToEuro, CurrencyConverter currencyConverter) {
+    public Country(String name, double vatPercentage, String currencySymbol) {
         this.name = name;
         this.vatPercentage = vatPercentage;
         this.currencySymbol = currencySymbol;
+    }
+
+    public abstract double convertToLocalCurrency(double priceInEuros);
+
+    public String getName() { return name; }
+    public double getVatPercentage() { return vatPercentage; }
+    public String getCurrencySymbol() { return currencySymbol; }
+}
+
+class EuroCountry extends Country {
+    public EuroCountry(String name, double vatPercentage, String currencySymbol) {
+        super(name, vatPercentage, currencySymbol);
+    }
+
+    @Override
+    public double convertToLocalCurrency(double priceInEuros) {
+        return priceInEuros;
+    }
+}
+
+class NonEuroCountry extends Country {
+    private final double exchangeRateToEuro;
+
+    public NonEuroCountry(String name, double vatPercentage, String currencySymbol, double exchangeRateToEuro) {
+        super(name, vatPercentage, currencySymbol);
         this.exchangeRateToEuro = exchangeRateToEuro;
-        this.currencyConverter = currencyConverter;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public double getVatPercentage() {
-        return vatPercentage;
-    }
-
-    public String getCurrencySymbol() {
-        return currencySymbol;
+    @Override
+    public double convertToLocalCurrency(double priceInEuros) {
+        return priceInEuros * exchangeRateToEuro;
     }
 
     public double getExchangeRateToEuro() {
         return exchangeRateToEuro;
-    }
-
-    public CurrencyConverter getCurrencyConverter() {
-        return currencyConverter;
     }
 }
 
@@ -94,20 +95,20 @@ class ARPMSystem {
 
     public void showPricesForProduct(Product product) {
         System.out.println("\nPrices for product: " + product.getName() + "\n");
-
+    
         System.out.printf("%-25s %20s %20s %25s %30s%n", "Country", "Price excl. VAT (€)", "Price incl. VAT (€)", "Price in Local Currency", "VAT Amount (€ / Local)");
         System.out.println("------------------------------------------------------------------------------------------------------------------------------");
-
+    
         for (Country country : countries) {
             double basePriceEuros = product.getPriceExclusiveVAT();
             double priceWithVAT = VATCalculator.calculatePriceWithVAT(basePriceEuros, country.getVatPercentage());
             double vatAmountEuro = priceWithVAT - basePriceEuros;
-            double localPrice = country.getCurrencyConverter().convertToLocalCurrency(priceWithVAT, country.getExchangeRateToEuro());
-            double vatAmountLocal = vatAmountEuro * country.getExchangeRateToEuro();
-
-            System.out.printf("%-25s %20.2f %20.2f %s%-23.2f %10.2f / %s%-14.2f%n", country.getName(), basePriceEuros, priceWithVAT, country.getCurrencySymbol(), localPrice, vatAmountEuro, country.getCurrencySymbol(), vatAmountLocal);
+            double localPrice = country.convertToLocalCurrency(priceWithVAT);
+            double vatAmountLocal = country.convertToLocalCurrency(vatAmountEuro);
+    
+            System.out.printf("%-25s %20.2f %20.2f %s%-23.2f %10.2f / %s%-14.2f%n",country.getName(), basePriceEuros, priceWithVAT, country.getCurrencySymbol(), localPrice, vatAmountEuro, country.getCurrencySymbol(), vatAmountLocal);
         }
-    }
+    }    
 }
 
 class Main {
@@ -115,40 +116,40 @@ class Main {
         Scanner scanner = new Scanner(System.in);
         ARPMSystem arpm = new ARPMSystem();
 
-        arpm.addCountry(new Country("Austria", 10.0, "€", 1.00, new EuroToCurrencyConverter()));
-        arpm.addCountry(new Country("Albania", 20.0, "L", 99.40, new EuroToCurrencyConverter()));
-        arpm.addCountry(new Country("Belgium", 6.0, "€", 1.00, new EuroToCurrencyConverter()));
-        arpm.addCountry(new Country("Bulgaria", 9.0, "€", 1.00, new EuroToCurrencyConverter()));
-        arpm.addCountry(new Country("Bosnia and Herzegovina", 17.0, "KM", 1.96, new EuroToCurrencyConverter()));
-        arpm.addCountry(new Country("Croatia", 13.0, "€", 1.00, new EuroToCurrencyConverter()));
-        arpm.addCountry(new Country("Cyprus", 5.0, "€", 1.00, new EuroToCurrencyConverter()));
-        arpm.addCountry(new Country("Czechia", 12.0, "KČ", 25.01, new EuroToCurrencyConverter()));
-        arpm.addCountry(new Country("Denmark", 25.0, "KR", 7.47, new EuroToCurrencyConverter()));
-        arpm.addCountry(new Country("Estonia", 22.0, "€", 1.00, new EuroToCurrencyConverter()));
-        arpm.addCountry(new Country("Finland", 14.0, "€", 1.00, new EuroToCurrencyConverter()));
-        arpm.addCountry(new Country("France", 10.0, "€", 1.00, new EuroToCurrencyConverter()));
-        arpm.addCountry(new Country("Germany", 7.0, "€", 1.00, new EuroToCurrencyConverter()));
-        arpm.addCountry(new Country("Greece", 13.0, "€", 1.00, new EuroToCurrencyConverter()));
-        arpm.addCountry(new Country("Hungary", 5.0, "FT", 407.58, new EuroToCurrencyConverter()));
-        arpm.addCountry(new Country("Ireland", 0.0, "€", 1.00, new EuroToCurrencyConverter()));
-        arpm.addCountry(new Country("Italy", 5.0, "€", 1.00, new EuroToCurrencyConverter()));
-        arpm.addCountry(new Country("Kosovo", 8.0, "€", 1.00, new EuroToCurrencyConverter()));
-        arpm.addCountry(new Country("Latvia", 12.0, "€", 1.00, new EuroToCurrencyConverter()));
-        arpm.addCountry(new Country("Lithuania", 21.0, "€", 1.00, new EuroToCurrencyConverter()));
-        arpm.addCountry(new Country("Luxembourg", 3.0, "€", 1.00, new EuroToCurrencyConverter()));
-        arpm.addCountry(new Country("Malta", 0.0, "€", 1.00, new EuroToCurrencyConverter()));
-        arpm.addCountry(new Country("Montenegro", 7.0, "€", 1.00, new EuroToCurrencyConverter()));
-        arpm.addCountry(new Country("North Macedonia", 5.0, "ДЕН", 61.54, new EuroToCurrencyConverter()));
-        arpm.addCountry(new Country("Portugal", 6.0, "€", 1.00, new EuroToCurrencyConverter()));
-        arpm.addCountry(new Country("Serbia", 10.0, "DIN", 117.25, new EuroToCurrencyConverter()));
-        arpm.addCountry(new Country("Slovakia", 5.0, "€", 1.00, new EuroToCurrencyConverter()));
-        arpm.addCountry(new Country("Slovenia", 9.5, "€", 1.00, new EuroToCurrencyConverter()));
-        arpm.addCountry(new Country("Spain", 4.0, "€", 1.00, new EuroToCurrencyConverter()));
-        arpm.addCountry(new Country("Sweden", 12.0, "KR", 11.10, new EuroToCurrencyConverter()));
-        arpm.addCountry(new Country("Switzerland", 2.6, "₣", 0.93, new EuroToCurrencyConverter()));
-        arpm.addCountry(new Country("The Netherlands", 8.0, "€", 1.00, new EuroToCurrencyConverter()));
-        arpm.addCountry(new Country("United Kingdom", 0.0, "£", 0.86, new EuroToCurrencyConverter()));
-        arpm.addCountry(new Country("United States", 0.0, "$", 1.14, new EuroToCurrencyConverter()));
+        arpm.addCountry(new EuroCountry("Austria", 10.0, "€"));
+        arpm.addCountry(new NonEuroCountry("Albania", 20.0, "L", 99.40));
+        arpm.addCountry(new EuroCountry("Belgium", 6.0, "€"));
+        arpm.addCountry(new EuroCountry("Bulgaria", 9.0, "€"));
+        arpm.addCountry(new NonEuroCountry("Bosnia and Herzegovina", 17.0, "KM", 1.96));
+        arpm.addCountry(new EuroCountry("Croatia", 13.0, "€"));
+        arpm.addCountry(new EuroCountry("Cyprus", 5.0, "€"));
+        arpm.addCountry(new NonEuroCountry("Czechia", 12.0, "KČ", 25.01));
+        arpm.addCountry(new NonEuroCountry("Denmark", 25.0, "KR", 7.47));
+        arpm.addCountry(new EuroCountry("Estonia", 22.0, "€"));
+        arpm.addCountry(new EuroCountry("Finland", 14.0, "€"));
+        arpm.addCountry(new EuroCountry("France", 10.0, "€"));
+        arpm.addCountry(new EuroCountry("Germany", 7.0, "€"));
+        arpm.addCountry(new EuroCountry("Greece", 13.0, "€"));
+        arpm.addCountry(new NonEuroCountry("Hungary", 5.0, "FT", 407.58));
+        arpm.addCountry(new EuroCountry("Ireland", 0.0, "€"));
+        arpm.addCountry(new EuroCountry("Italy", 5.0, "€"));
+        arpm.addCountry(new EuroCountry("Kosovo", 8.0, "€"));
+        arpm.addCountry(new EuroCountry("Latvia", 12.0, "€"));
+        arpm.addCountry(new EuroCountry("Lithuania", 21.0, "€"));
+        arpm.addCountry(new EuroCountry("Luxembourg", 3.0, "€"));
+        arpm.addCountry(new EuroCountry("Malta", 0.0, "€"));
+        arpm.addCountry(new EuroCountry("Montenegro", 7.0, "€"));
+        arpm.addCountry(new NonEuroCountry("North Macedonia", 5.0, "ДЕН", 61.54));
+        arpm.addCountry(new EuroCountry("Portugal", 6.0, "€"));
+        arpm.addCountry(new NonEuroCountry("Serbia", 10.0, "DIN", 117.25));
+        arpm.addCountry(new EuroCountry("Slovakia", 5.0, "€"));
+        arpm.addCountry(new EuroCountry("Slovenia", 9.5, "€"));
+        arpm.addCountry(new EuroCountry("Spain", 4.0, "€"));
+        arpm.addCountry(new NonEuroCountry("Sweden", 12.0, "KR", 11.10));
+        arpm.addCountry(new NonEuroCountry("Switzerland", 2.6, "₣", 0.93));
+        arpm.addCountry(new EuroCountry("The Netherlands", 8.0, "€"));
+        arpm.addCountry(new NonEuroCountry("United Kingdom", 0.0, "£", 0.86));
+        arpm.addCountry(new NonEuroCountry("United States", 0.0, "$", 1.14));
 
         arpm.addProduct(new Product("Apples", 1.55));
         arpm.addProduct(new Product("Gouda cheese", 2.79));
